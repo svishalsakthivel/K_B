5bfb61ed4f6d8fe63be755c91034b9c042e4756b
vishal-s3 <vishal.s3@olakrutrim.com>
2024-01-24 17:20:35 +0530

    full aws CLI

--- build_1.py
+++ build_1.py
@@ -5,6 +5,5 @@ def download_s3_basket():
     os.mkdir("/root/vishal")
 
     # os.system("aws --profile llm s3 cp s3://llm-spark/silicon/tensorrtllm_backend/ /root/vishal/t_b --recursive")
-    # os.system("aws --profile llm s3 ls s3://llm-spark/silicon/tensorrtllm_backend/")
-    os.system("as3 ls s3://llm-spark/silicon/tensorrtllm_backend/")
+    os.system("aws --profile llm s3 ls s3://llm-spark/silicon/tensorrtllm_backend/")
     os.mkdir("/root/vishal/t_b")
