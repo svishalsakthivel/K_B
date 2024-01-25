c21f11de63e49ccefb6c3c7a6e796873db307be1
vishal-s3 <vishal.s3@olakrutrim.com>
2024-01-25 12:56:50 +0530

    costamized change

--- build_4.py
+++ build_4.py
@@ -2,9 +2,10 @@ import os
 
 
 def docker_build():
-    os.system("DOCKER_BUILDKIT=1 docker build -t triton_trt_llm -f dockerfile/Dockerfile.trt_llm_backend .")
+    # os.system("DOCKER_BUILDKIT=1 docker build -t triton_trt_llm -f dockerfile/Dockerfile.trt_llm_backend .")
+    os.system("DOCKER_BUILDKIT=1 docker build -t triton_trt_llm_vishal -f dockerfile/dockerfile.vishal_trt_llm .")
 
 
 def docker_run():
     os.system(
-        "docker run --rm -it --net host --shm-size=2g --ulimit memlock=-1 --ulimit stack=67108864  --gpus='"'device=0'"' -v /local/si/silicon/FT:/FT -v /local/s3/mountvolume:/dynamic -v /local/si/silicon/tensorrtllm_backend:/tensorrtllm_backend triton_trt_llm bash")
+        "docker run --rm -it --net host --shm-size=2g --ulimit memlock=-1 --ulimit stack=67108864  --gpus='"'device=0'"' -v /local/si/silicon/FT:/FT -v /local/s3/mountvolume:/dynamic -v /local/si/silicon/tensorrtllm_backend:/tensorrtllm_backend triton_trt_llm_vishal")
--- main_build.py
+++ main_build.py
@@ -4,7 +4,7 @@ import build_3 as b3
 import build_4 as b4
 
 b1.download_s3_basket()
-# b2.install_git_lfs()
+b2.install_git_lfs()
 b3.update_git_lfs_submodule()
 b4.docker_build()
-b4.docker_run()
+# b4.docker_run()
