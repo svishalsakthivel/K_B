f86ebcc05a21abb22238cb871c34c773de90c1b5
vishal-s3 <vishal.s3@olakrutrim.com>
2024-01-25 13:11:51 +0530

    location chenged

--- build_4.py
+++ build_4.py
@@ -3,7 +3,7 @@ import os
 
 def docker_build():
     # os.system("DOCKER_BUILDKIT=1 docker build -t triton_trt_llm -f dockerfile/Dockerfile.trt_llm_backend .")
-    os.system("DOCKER_BUILDKIT=1 docker build -t triton_trt_llm_vishal -f /root/yt/tensorrtllm_backend/dockerfile .")
+    os.system("DOCKER_BUILDKIT=1 docker build -t triton_trt_llm_vishal -f /root/yt/tensorrtllm_backend/dockerfile/Dockerfile.trt_llm_v .")
 
 
 def docker_run():
